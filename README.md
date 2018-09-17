985a1d9a411fb6da65c069d8a4bf1ab97564ab0c
Sooyong Wang <wangsy@wangsy.com>
2018-09-17 20:02:39 +0900

    Capistrano Puma

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # Capistrano gem
 
 https://github.com/capistrano/capistrano
+
+# Capistrano rbenv
+
+https://github.com/capistrano/rbenv
+
+# edit deploy.rb
+
+```
+set :application, "pumadeploy"
+set :repo_url, "git@github.com:wangsy/pumadeploy.git"
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :deploy_to, "/home/wangsy/pumadeploy"
+
+append :linked_files, "config/database.yml"
+
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+```
+
+# edit deploy/staging.rb
+
+```
+server "pumadeploy", user: "wangsy", roles: %w{app db web}
+```
+
+# push to github
+
+# first deploy
+
+`bundle exec cap staging deploy`
+
+# Capistrano puma
+
+https://github.com/seuros/capistrano-puma
+
+`bundle exec cap staging puma:config`
+`cap production puma:nginx_config
+`
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "pumadeploy"
+set :repo_url, "git@github.com:wangsy/pumadeploy.git"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.5.1'
@@ -11,7 +11,7 @@ set :rbenv_ruby, '2.5.1'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/wangsy/pumadeploy"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,10 +24,10 @@ set :rbenv_ruby, '2.5.1'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
